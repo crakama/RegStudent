12ad48359780eed9eb80ea3c3ab102e21c4617fc
crakama <crakama89@gmail.com>
2016-08-11 00:15:49 +0300

    Set on item selected listener for the spinner

--- app/src/main/java/com/crakama/admin/regstudent/SecondActivity.java
+++ app/src/main/java/com/crakama/admin/regstudent/SecondActivity.java
@@ -8,6 +8,6 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sec);
+        setContentView(R.layout.dropdownspin);
     }
 }
