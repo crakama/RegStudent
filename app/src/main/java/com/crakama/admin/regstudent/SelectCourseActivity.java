721626278ab9853dbd5e0119d50ffbf627a94b5f
crakama <crakama89@gmail.com>
2016-08-12 23:59:29 +0300

    Restructure course activity class

--- app/src/main/java/com/crakama/admin/regstudent/SelectCourseActivity.java
+++ app/src/main/java/com/crakama/admin/regstudent/SelectCourseActivity.java
@@ -2,6 +2,11 @@ package com.crakama.admin.regstudent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SelectCourseActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class SelectCourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_option);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.colors, R.layout.dropdownspin);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentadapterView, View view, int pos, long l) {
+                Toast.makeText(
+                        parentadapterView.getContext(),
+                        "The color is" + parentadapterView.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+
+            public void onNothingSelected(AdapterView<?> parentadapterView) {
+
+            }
+        });
     }
+
+
+
 }
